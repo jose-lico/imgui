03eaa8504af12fa1aa9554e6d8e478f0694f9f3c
José Lico <57019940+jose-lico@users.noreply.github.com>
2022-09-03 22:06:52 +0100

    premake5 change & folder structure

--- premake5.lua
+++ premake5.lua
@@ -1,17 +1,22 @@
 project "ImGui"
 	kind "StaticLib"
 	language "C++"
+	configmap {
+        ["ReleaseNoImGui"] = "Release"
+    }
 
-	targetdir ("bin/" .. outputDir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputDir .. "/%{prj.name}")
+	targetdir ("%{wks.location}/bin/" .. outputDir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputDir .. "/%{prj.name}")
+
+	includedirs "include/imgui"
 
 	files
 	{
-		"include/imgui/imgui.cpp",
-		"include/imgui/imgui_demo.cpp",
-		"include/imgui/imgui_draw.cpp",
-		"include/imgui/imgui_widgets.cpp",
-		"include/imgui/imgui_tables.cpp",
+		"imgui.cpp",
+		"imgui_demo.cpp",
+		"imgui_draw.cpp",
+		"imgui_widgets.cpp",
+		"imgui_tables.cpp",
 		
 		"include/imgui/imconfig.h",
 		"include/imgui/imgui.h",
