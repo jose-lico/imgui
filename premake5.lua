cfb84406744dfe48ef86e6b470e90209f7ca4e38
José Lico <57019940+jose-lico@users.noreply.github.com>
2022-11-30 19:06:04 +0000

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,9 +1,11 @@
 project "ImGui"
 	kind "StaticLib"
 	language "C++"
-	configmap {
-        ["ReleaseNoImGui"] = "Release"
-    }
+	configmap 
+	{
+		["ReleaseWithInfo"] = "Release"
+		["ReleaseNoImGui"] = "Release"
+	}
 
 	targetdir ("%{wks.location}/bin/" .. outputDir .. "/%{prj.name}")
 	objdir ("%{wks.location}/bin-int/" .. outputDir .. "/%{prj.name}")
@@ -43,4 +45,3 @@ project "ImGui"
 	filter "configurations:Release"
 		runtime "Release"
 		optimize "On"
-		
